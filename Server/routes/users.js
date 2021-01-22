6ac0e1175ed7a41e33bb0beef6d4a21e51696851
joon <naraankr77@icloud.com>
2021-01-22 23:38:02 +0900

    서버(로그인 기능 오류 수정)

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const user = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.send('회원가입 화면');
@@ -45,35 +46,36 @@ router.get('/login', (req,res) => {
 
 router.post('/login', (req,res) => {
 //jwt 토큰 발급 
-    
+//클라이언트는 토큰을 저장하고, 로그인이 필요한 API 요청시 JWT를 헤더에 포함하고
+//서버에 전송, 서버는 디코딩하고 서비스 제공
     const email = req.body.email;
     const password = req.body.password;
     //console.log(email,password);
 
-    const user = User.findOne({email});
-    if(!user){
-        return res.status(400).json({
-            msg : "존재 하지 않는 이메일"
-        });
-    }
-
-    bcrypt.compare(password, user.password)
-    .then(isMatch => {
-        if(isMatch){
-            const payload = {
-                email : user.email
-            };
-        const token = jwt.sign(payload, process.env.JWT_SECRET).res.json({
-            token
-        });
-        }
-        else{
+    User.findOne({email}, (error, user) => {
+        if(!user){
             return res.status(400).json({
-                msg : "패스워드가 일치하지 않음"
+                msg : "존재 하지 않는 이메일"
             });
         }
-    })
-    
+        else if(user){
+            bcrypt.compare(req.body.password, user.password, (error,isMatch) =>{
+                if(error){
+                    return res.status(500).json({error:'error'});
+                }
+                if(isMatch){
+                    const payload = {
+                        email : email
+                    };
+                    const token = jwt.sign(payload,process.env.JWT_SECRET);
+                    res.status(200).json({msg:"로그인 성공", token});
+                }
+                else{
+                    return res.status(400).json({msg : "패스워드가 일치하지 않음" });
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
